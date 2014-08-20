1499ec08f80710194e0e4dd230976c1d52b196d8
reza <reza.energy@gmail.com>
2014-08-21 01:45:25 +0430

    Update rule_maker.py

--- rule_maker.py
+++ rule_maker.py
@@ -4,16 +4,16 @@
 # -*- coding: utf-8 -*-
 import codecs
 dict={
-    u"پیاده" :u"روی|رو|نظام",
+u"تام":u"الاختیار",
 }
 vari=u'''
-<!-- Farsi rule, 2014-08-14 -->
-<rule id="%s" name="%s">
+<rule>
  <pattern>
-  <token regexp='yes'>(%s)+</token>
-  <token regexp='yes'>(%s)</token>
+  <%s>%s</token>
+  <%s>%s</token>
  </pattern>
- <message>%s</message>
+<message>آیا منظور شما <suggestion>\1&#8204;\2</suggestion>است؟</message>
+ <short>%s</short>
  <example type='incorrect'><marker>%s</marker></example>
  <example type='correct'>%s</example>
 </rule>'''
@@ -21,7 +21,21 @@ count=0
 text=u'\n'
 for i in dict:
     count+=1
-    our_vari=vari %(u'ZwnjCorection_ID_'+str(count),u'ZwnjCorection'+str(count),i,dict[i],u'اصلاح نیم‌فاصلهٔ میان دو کلمه',i.split(u'|')[0]+u' '+dict[i].split(u'|')[0],i.split(u'|')[0]+u'‌'+dict[i].split(u'|')[0])
+    if u'|' in i:
+        item1=u'('+i+u')'
+        case1="token regexp='yes'"
+    else:
+        item1=i
+        case1="token"
+
+    if u'|' in dict[i]:
+        item2=u'('+dict[i]+u')'
+        case2="token regexp='yes'"
+    else:
+        item2=dict[i]
+        case2="token"
+
+    our_vari=vari %(case1,item1,case2,item2,u'اصلاح فاصلهٔ مجازی میان کلمهٔ مرکب',item1.split(u'|')[0].replace(u'(',u'').replace(u')',u'').strip()+u' '+item2.split(u'|')[0].replace(u'(',u'').replace(u')',u'').strip(),item1.split(u'|')[0].replace(u'(',u'').replace(u')',u'').strip()+u'‌'+item2.split(u'|')[0].replace(u'(',u'').replace(u')',u'').strip())
     text+=our_vari
 with codecs.open( u'my_patch.txt',mode = 'w',encoding = 'utf8' ) as f:
-                    f.write( text )
+    f.write( text )
